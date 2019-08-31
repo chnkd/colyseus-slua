7c1e163170ecc23f79dc7458a9d8c1d95e9ef5ac
Chen Siquan <406908+chsqn@users.noreply.github.com>
2019-08-31 14:26:21 +0800

    增加游戏的启动回调和结束回调。

---
+++ Assets/Scripts/Coord.cs
@@ -0,0 +1,35 @@
+#pragma warning disable RECS0016 // Bitwise operation on enum which has no [Flags] attribute
+#pragma warning disable IDE0058 // Use discarded local
+
+using System.IO;
+using UnityEngine;
+using SLua;
+
+public class Coord : MonoBehaviour
+{
+    private LuaTable game;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        LuaSvr luaSvr = new LuaSvr();
+#if UNITY_EDITOR
+        LuaSvr.mainState.loaderDelegate += (string file, ref string absolutePath) =>
+        {
+            absolutePath = Application.dataPath + "/" + file.Replace('.', '/') + ".lua";
+            return File.ReadAllBytes(absolutePath);
+        };
+#endif
+        luaSvr.init((completion) => Debug.Log(completion), () =>
+        {
+            luaSvr.start("Scripts/entry");
+            game = LuaSvr.mainState.getTable("Game");
+            game.invoke("Start", game);
+        }, LuaSvrFlag.LSF_BASIC | LuaSvrFlag.LSF_EXTLIB);
+    }
+
+    private void OnDestroy()
+    {
+        game.invoke("Finish", game);
+    }
+}
---
+++ Assets/Scripts/game.lua
@@ -0,0 +1,5 @@
+Game = {}
+function Game:Start()
+end
+function Game:Finish()
+end
