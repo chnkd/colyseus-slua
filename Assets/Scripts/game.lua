4680fe0478c04ea8b6a28e3d6781bb0517b153cd
Chen Siquan <406908+chsqn@users.noreply.github.com>
2019-12-22 20:55:56 +0800

    連接服務器範例。

--- Assets/Scripts/game.lua
+++ Assets/Scripts/game.lua
@@ -1,5 +1,17 @@
+local Colyseus = require('Scripts/lib/colyseus.client')
 Game = {}
 function Game:Start()
+  local client = Colyseus.new("ws://rzcap:41003")
+  self.rooms = {}
+  client:join_or_create('battle', function(err, room)
+    if err then
+      return
+    end
+    table.insert(self.rooms, room)
+  end)
 end
 function Game:Finish()
+  for i=1, #self.rooms do
+    self.rooms[i]:leave(false)
+  end
 end
