9b5f4fed3a3a068bc4e4ee28ae102f8251dcf02b
Chen Siquan <406908+chsqn@users.noreply.github.com>
2020-05-10 22:36:04 +0800

    Fix style.

--- Assets/Scripts/game.lua
+++ Assets/Scripts/game.lua
@@ -1,14 +1,14 @@
 local Colyseus = require('Scripts/lib/colyseus.client')
 Game = {}
 function Game:Start()
-  local client = Colyseus.new("ws://localhost:41003")
+  local client = Colyseus.new('ws://localhost:41003')
   self.rooms = {}
   client:join_or_create('Battle', function(err, room)
     if err then
       return
     end
-    room:on("statechange", function(state)
-      print("new state:", require('Scripts/lib/inspect')(state))
+    room:on('statechange', function(state)
+      print('new state:', require('Scripts/lib/inspect')(state))
     end)
     table.insert(self.rooms, room)
   end)
