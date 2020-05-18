388f643193931e61230dd17ba6ed19b02ed464db
Chen Siquan <406908+chsqn@users.noreply.github.com>
2020-05-18 23:06:15 +0800

    Reserve the seat.

--- App/server.js
+++ App/server.js
@@ -3,6 +3,9 @@ const app = new (require('colyseus').Server)({
 })
 class Battle extends require('colyseus').Room {
   onCreate (options) {
+    this.autoDispose = false
+    this.maxClients = options.maxClients
+    this.reservedTime = options.reservedTime
     const { Schema, defineTypes } = require('@colyseus/schema')
     class Sync extends Schema {
       constructor () {
@@ -15,8 +18,36 @@ class Battle extends require('colyseus').Room {
     })
     this.setState(new Sync())
   }
+  async onLeave (client, consented) {
+    console.info('leaved')
+    var isRemoval
+    if (consented) {
+      isRemoval = !this.autoDispose
+    } else {
+      console.info('reserved')
+      isRemoval = await this.allowReconnection(client, this.reservedTime).then(function () {
+        console.info('rejoined')
+        return false
+      }.bind(this)).catch(function () {
+        if (this.autoDispose) {
+          return false
+        } else {
+          return true
+        }
+      }.bind(this))
+    }
+    if (isRemoval) {
+      this.disconnect()
+    }
+  }
+  async onDispose () {
+    console.info('dispose')
+  }
 }
-app.define('Battle', Battle)
+app.define('Battle', Battle, {
+  maxClients: 1,
+  reservedTime: 15
+})
 app.onShutdown(async () => {
   console.info('close')
 })
