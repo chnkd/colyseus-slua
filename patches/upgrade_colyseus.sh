b006b8acda8aa2463616ab1adec4b96b6f507625
Chen Siquan <406908+chsqn@users.noreply.github.com>
2020-10-29 20:22:01 +0800

    Fix upgrader

--- patches/upgrade_colyseus.sh
+++ patches/upgrade_colyseus.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 wd=$(dirname $0)
-myfile=$wd/../Assets/Scripts/lib/colyseus
+my=$wd/../Assets/Scripts/lib/colyseus
 version=$wd/version.txt
 function rev() {
   git clone -q https://github.com/colyseus/colyseus-defold.git $1
@@ -15,8 +15,19 @@ function rev() {
 function dir() {
   echo $wd/$*/colyseus
 }
-cur=$(cat $version)
-echo $(rev youfile $(rev oldfile $cur^)) > $version
-#for file in $(gfind $myfile -name "*.lua" -printf "%P\n"); do
-#  diff3 -m $myfile/$file $(dir oldfile)/$file $(dir youfile)/$file | sponge $myfile/$file
-#done
+function list() {
+  gfind $* -name "*.lua" -printf "%P\n"
+}
+#echo $(rev you $(rev old $(cat $version)^)) > $version
+old=$(list $my | sort)
+new=$(list $(dir you) | sort)
+for file in $(comm -23 <(echo "$old") <(echo "$new")); do
+  rm $my/$file
+done
+for file in $(comm -13 <(echo "$old") <(echo "$new")); do
+  install $(dir you)/$file $my
+done
+for file in $(comm -12 <(echo "$old") <(echo "$new")); do
+  myfile=$my/$file
+  diff3 -mE $myfile $(dir old)/$file $(dir you)/$file | sponge $myfile
+done
