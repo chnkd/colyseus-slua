fcf6a8cfe4dabf557e6e1b2f1add0966aed01904
Chen Siquan <406908+chsqn@users.noreply.github.com>
2020-10-29 17:55:41 +0800

    Upgrade to next commit

--- patches/upgrade_colyseus.sh
+++ patches/upgrade_colyseus.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
-if [ -z "$*" ]; then
-  echo "$0 <MYFILE> <OLDFILE> <YOURFILE>"
-else
-  wd=$(dirname $0)
-  cur=$wd/$1/colyseus
-  function rev() {
-    echo $wd/../../$*/colyseus
-  }
-  for file in $(gfind $cur -name "*.lua" -printf "%P\n"); do
-    diff3 -m $cur/$file $(rev $2)/$file $(rev $3)/$file | sponge $cur/$file
-  done
-fi
+wd=$(dirname $0)
+myfile=$wd/../Assets/Scripts/lib/colyseus
+version=$wd/version.txt
+function rev() {
+  git clone -q https://github.com/colyseus/colyseus-defold.git $1
+  pushd . > /dev/null
+  cd $1
+  id=$(git log --reverse --ancestry-path $2..master --oneline | head -1 | cut -d' ' -f1)
+  echo $id
+  git reset -q --hard $id
+  popd > /dev/null
+}
+function dir() {
+  echo $wd/$*/colyseus
+}
+cur=$(cat $version)
+echo $(rev youfile $(rev oldfile $cur^)) > $version
+#for file in $(gfind $myfile -name "*.lua" -printf "%P\n"); do
+#  diff3 -m $myfile/$file $(dir oldfile)/$file $(dir youfile)/$file | sponge $myfile/$file
+#done
